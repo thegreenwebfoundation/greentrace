9fab8d82eb530d775722103b55d3c17a790f91ae
Chris Adams <chris@productscience.co.uk>
2019-07-14 16:33:27 +0200

    Initial commit to set up app

---
+++ src/renderer/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <link href="https://api.tiles.mapbox.com/mapbox-gl-js/v1.1.0/mapbox-gl.css" rel="stylesheet" />
+
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "greentrace"
+};
+</script>
+
+
+
+<style>
+/* CSS */
+</style>
---
+++ src/renderer/components/LandingPage.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="wrapper">
+    <main>
+      <arc-map></arc-map>
+    </main>
+  </div>
+</template>
+
+<script>
+import SystemInformation from "./LandingPage/SystemInformation";
+import ArcMap from "./LandingPage/ArcMap";
+
+export default {
+  name: "landing-page",
+  components: { SystemInformation, ArcMap },
+  methods: {}
+};
+</script>
+
+<style>
+</style>
