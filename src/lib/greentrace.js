f4459291409f6df1615669010744fda48488a4bf
Chris Adams <chris@productscience.co.uk>
2019-07-14 16:34:43 +0200

    Add greentrace with test harness

---
+++ src/lib/greentrace.js
@@ -0,0 +1,4 @@
+import debugLib from "debug"
+const debug = debugLib("tgwf:greenTrace")
+// TODO add code for
+debug("Code for the page checker goes here. We needn't load up all the vue and electron gubbins")
---
+++ src/lib/greentrace.test.js
@@ -0,0 +1,15 @@
+import { expect } from 'chai';
+
+import PageCheck from "./greentrace.js"
+
+describe("GreenTrace", () => {
+  describe("PageCheck", () => {
+    it("fetches html content from a page")
+    it("parses out href links from the dom")
+    it("pulls the domain from the hrefs")
+    it("builds a collection of domains from hrefs")
+  })
+
+  // the actual building of traceroutes, once we have a list of domains is handled by the greentrace-lib
+
+})
